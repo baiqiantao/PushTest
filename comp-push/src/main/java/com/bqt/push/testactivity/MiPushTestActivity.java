9b061ee13fc081b0a6a51c41530c2149b9f8d290
baiqiantao <baiqiantao@sina.com>
2018-04-18 17:57:07 +0800

    集成小米推送

--- comp-push/src/main/java/com/bqt/push/testactivity/MiPushTestActivity.java
+++ comp-push/src/main/java/com/bqt/push/testactivity/MiPushTestActivity.java
@@ -21,8 +21,8 @@ import java.util.Arrays;
 
 /**
  * 1、设置 topic 和 alias。 服务器端使用 appsecret 即可以向demo发送广播和单点的消息。<br/>
- * 2、为了修改本 demo 为使用你自己的 appid，你需要修改几个地方：DemoApplication.java 中的 APP_ID,
- * APP_KEY，AndroidManifest.xml 中的 packagename，和权限 permission.MIPUSH_RECEIVE 的前缀为你的 packagename。
+ * 2、为了修改本 demo 为使用你自己的 appid，你需要修改几个地方：Application中的 APP_ID 和 APP_KEY
+ *      AndroidManifest.xml 中的 packagename，和权限 permission.MIPUSH_RECEIVE 的前缀为你的 packagename。
  */
 public class MiPushTestActivity extends ListActivity {
 	
